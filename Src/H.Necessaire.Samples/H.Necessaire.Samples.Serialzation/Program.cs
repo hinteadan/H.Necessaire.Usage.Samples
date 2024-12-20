3efe7474299841f02d670181681bf7e917972428
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2024-12-20 11:34:30 +0200

    JSON Serialization Samples

---
+++ Src/H.Necessaire.Samples/H.Necessaire.Samples.Serialzation/Program.cs
@@ -0,0 +1,22 @@
+﻿using H.Necessaire.Serialization;
+
+namespace H.Necessaire.Samples.Serialzation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SomeData data = new SomeData { Name = "Hintee" };
+
+            SomeData[] dataArray = [data, data];
+
+            string jsonObjectString = data.ToJsonObject();
+
+            string jsonArrayString = dataArray.ToJsonArray();
+
+            SomeData deserializedData = jsonObjectString.JsonToObject<SomeData>();
+
+            SomeData[] deserializedArray = jsonArrayString.JsonToObject<SomeData[]>();
+        }
+    }
+}
---
+++ Src/H.Necessaire.Samples/H.Necessaire.Samples.Serialzation/SomeData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace H.Necessaire.Samples.Serialzation
+{
+    internal class SomeData
+    {
+        public Guid ID { get; set; }
+        public DateTime AsOf { get; set; } = DateTime.UtcNow;
+        public string Name { get; set; }
+        public Note[] Attributes { get; set; }
+    }
+}
