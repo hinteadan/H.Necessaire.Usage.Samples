00abd9fd0cd3b9b667325c528f4d8967a4367f16
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2024-12-21 15:30:22 +0200

    OD: Resilience Model

---
+++ Src/H.Necessaire.Samples/H.Necessaire.Samples.Resilience/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+
+namespace H.Necessaire.Samples.Resilience
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            OperationResult result = await DoSomeWorkAlignedWithResilientPattern(logger: null);
+        }
+
+        static async Task<OperationResult> DoSomeWorkAlignedWithResilientPattern(ImALogger logger)
+        {
+            if (logger is null)
+                return OperationResult.Fail($"{nameof(logger)} is null. A logger must be provided.");
+
+            await logger.LogInfo("Doing some work");
+
+            return OperationResult.Win();
+        }
+    }
+}
