c309ed60af11b1509b5afdcbeeb01cde7cb1be05
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2024-12-23 14:03:11 +0200

    Throttling and Debouncing Taming samples

---
+++ Src/H.Necessaire.Samples/H.Necessaire.Samples.ThrottleAndDebounce/HighFrequencyEventDaemon.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace H.Necessaire.Samples.ThrottleAndDebounce
+{
+    internal class HighFrequencyEventDaemon : ImADaemon, ImADependency
+    {
+        static readonly TimeSpan eventFrequency = TimeSpan.FromMilliseconds(15);
+        readonly List<AsyncEventHandler<EventArgs>> onHighFrequencyEventHandlers = new List<AsyncEventHandler<EventArgs>>();
+        public event AsyncEventHandler<EventArgs> OnHighFrequencyEvent
+        {
+            add
+            {
+                if (value is null)
+                    return;
+
+                lock (onHighFrequencyEventHandlers)
+                {
+                    onHighFrequencyEventHandlers.Add(value);
+                }
+            }
+            remove
+            {
+                if (value is null)
+                    return;
+
+                lock (onHighFrequencyEventHandlers)
+                {
+                    onHighFrequencyEventHandlers.Remove(value);
+                }
+            }
+        }
+        ImAPeriodicAction frequentEventPeriodicAction;
+        public void ReferDependencies(ImADependencyProvider dependencyProvider)
+        {
+            frequentEventPeriodicAction = dependencyProvider.Get<ImAPeriodicAction>();
+        }
+
+        public Task Start(CancellationToken? cancellationToken = null)
+        {
+            frequentEventPeriodicAction.Start(eventFrequency, RaiseHighFrequencyEvent);
+
+            return Task.CompletedTask;
+        }
+
+        public Task Stop(CancellationToken? cancellationToken = null)
+        {
+            frequentEventPeriodicAction?.Stop();
+
+            return Task.CompletedTask;
+        }
+
+        async Task RaiseHighFrequencyEvent()
+        {
+            await
+                new Func<Task>(async () =>
+                {
+                    if (onHighFrequencyEventHandlers.IsEmpty())
+                        return;
+
+                    await Task.WhenAll(
+                        onHighFrequencyEventHandlers.Select(handler =>
+                            handler.Invoke(
+                                this,
+                                EventArgs.Empty
+                            )
+                        )
+                    );
+                })
+                .TryOrFailWithGrace(onFail: ex => { });
+        }
+
+
+    }
+}
---
+++ Src/H.Necessaire.Samples/H.Necessaire.Samples.ThrottleAndDebounce/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace H.Necessaire.Samples.ThrottleAndDebounce
+{
+    internal class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            HighFrequencyEventDaemon highFrequencyEventDaemon = IoC.NewDependencyRegistry()
+                .Register<HNecessaireDependencyGroup>(() => new HNecessaireDependencyGroup())
+                .Register<HighFrequencyEventDaemon>(() => new HighFrequencyEventDaemon())
+                .Get<HighFrequencyEventDaemon>()
+                ;
+
+            highFrequencyEventDaemon.OnHighFrequencyEvent += NormalExecution;
+            highFrequencyEventDaemon.OnHighFrequencyEvent += ThrottledExecution;
+            highFrequencyEventDaemon.OnHighFrequencyEvent += DebouncedExecution;
+
+            Console.WriteLine("Running... press Ctrl+C to stop.");
+
+            await highFrequencyEventDaemon.Start();
+
+            StopDaemonAfter(highFrequencyEventDaemon, TimeSpan.FromSeconds(3));            
+
+            await KeepConsoleAlive();
+        }
+
+        static async Task NormalExecution(object sender, EventArgs ev)
+        {
+            await Console.Out.WriteAsync('_');
+        }
+
+        static readonly Throttler throttler = new Throttler(async () => await Console.Out.WriteAsync("_Tht"), TimeSpan.FromSeconds(.35));
+        static async Task ThrottledExecution(object sender, EventArgs ev)
+        {
+            await throttler.Invoke();
+        }
+
+        static readonly Debouncer debouncer = new Debouncer(async () => await Console.Out.WriteAsync("_Dbc"), TimeSpan.FromSeconds(.35));
+        static async Task DebouncedExecution(object sender, EventArgs ev)
+        {
+            await debouncer.Invoke();
+        }
+
+        static void StopDaemonAfter(ImADaemon daemon, TimeSpan timeSpan)
+        {
+            Task.Run(async () =>
+            {
+                await Task.Delay(timeSpan);
+                await daemon.Stop();
+            });
+        }
+
+        static Task KeepConsoleAlive()
+        {
+            TaskCompletionSource taskCompletionSource = new TaskCompletionSource();
+
+            Console.CancelKeyPress += (sender, ev) =>
+            {
+                taskCompletionSource.SetResult();
+                Console.WriteLine($"Done @ {DateTime.Now}");
+            };
+
+            return taskCompletionSource.Task;
+        }
+    }
+}
