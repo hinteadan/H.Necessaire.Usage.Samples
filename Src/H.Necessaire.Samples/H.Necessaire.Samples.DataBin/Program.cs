0a60f66297d8375ddc975f0079c17eb91537398f
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2025-01-30 16:49:06 +0200

    DataBin Sample

---
+++ Src/H.Necessaire.Samples/H.Necessaire.Samples.DataBin/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace H.Necessaire.Samples.DataBin
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            DataBinMeta attachmentMeta = new()
+            {
+                Name = "attachment.png",
+                Format = new DataBinFormatInfo
+                {
+                    ID = "ImagePng",
+                    Extension = "png",
+                    MimeType = "image/png",
+                    Encoding = null,
+                },
+            };
+
+            H.Necessaire.DataBin attachment
+                = attachmentMeta
+                .ToBin(
+                    x => x.Name.OpenEmbeddedResource().ToDataBinStream().AsTask()
+                );
+
+            using (ImADataBinStream data = await attachment.OpenDataBinStream())
+            {
+                Console.WriteLine(await data.DataStream.ReadAsStringAsync());
+            }
+        }
+    }
+}
