c192390716c47530ec8ca9b462dab2ec75f792fa
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2025-01-31 12:03:14 +0200

    The very basic CLI App Sample

---
+++ Src/H.Necessaire.Samples/H.Necessaire.Samples.Runtime.CLI.Basic/HelloWorldCommand.cs
@@ -0,0 +1,16 @@
+﻿using H.Necessaire.CLI.Commands;
+using System;
+using System.Threading.Tasks;
+
+namespace H.Necessaire.Samples.Runtime.CLI.Basic
+{
+    internal class HelloWorldCommand : CommandBase
+    {
+        public override Task<OperationResult> Run()
+        {
+            Console.WriteLine("Hello from the lovely H.Necessaire CLI 💖!");
+
+            return OperationResult.Win().AsTask();
+        }
+    }
+}
---
+++ Src/H.Necessaire.Samples/H.Necessaire.Samples.Runtime.CLI.Basic/Program.cs
@@ -0,0 +1,17 @@
+﻿using H.Necessaire.CLI;
+using H.Necessaire.Runtime.CLI;
+using System.Threading.Tasks;
+
+namespace H.Necessaire.Samples.Runtime.CLI.Basic
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await new CliApp()
+                .WithEverything()
+                .Run()
+                ;
+        }
+    }
+}
